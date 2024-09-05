b50901f5dc9f2e9052e4c667790acc7f15311c83
Djalves424 <djalves424@gmail.com>
2024-09-05 17:34:34 -0300

    Adjusting some settings in the code

---
+++ src/main/java/com/devsuperior/workshopmongo/controllers/PostController.java
@@ -0,0 +1,48 @@
+package com.devsuperior.workshopmongo.controllers;
+
+import com.devsuperior.workshopmongo.controllers.util.URL;
+import com.devsuperior.workshopmongo.dto.PostDTO;
+import com.devsuperior.workshopmongo.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
+import java.text.ParseException;
+import java.time.Instant;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostController {
+
+	@Autowired
+	private PostService service;
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<PostDTO> findById(@PathVariable String id) {
+		PostDTO dto = service.findById(id);
+		return ResponseEntity.ok(dto);
+	}
+	
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<PostDTO>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) throws UnsupportedEncodingException {
+		text = URL.decodeParam(text);
+		List<PostDTO> list = service.findByTitle(text);
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value = "/fullsearch")
+	public ResponseEntity<List<PostDTO>> fullSearch(
+			@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate) throws UnsupportedEncodingException, ParseException {
+		
+		text = URL.decodeParam(text);
+		Instant min = URL.convertDate(minDate, Instant.EPOCH);
+		Instant max = URL.convertDate(maxDate, Instant.now());
+		
+		List<PostDTO> list = service.fullSearch(text, min, max);
+		return ResponseEntity.ok(list);
+	}
+}
---
+++ src/main/java/com/devsuperior/workshopmongo/controllers/UserController.java
@@ -0,0 +1,57 @@
+package com.devsuperior.workshopmongo.controllers;
+
+import com.devsuperior.workshopmongo.dto.PostDTO;
+import com.devsuperior.workshopmongo.dto.UserDTO;
+import com.devsuperior.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+	@Autowired
+	private UserService service;
+
+	@GetMapping
+	public ResponseEntity<List<UserDTO>> findAll() {
+		List<UserDTO> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		UserDTO dto = service.findById(id);
+		return ResponseEntity.ok(dto);
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<PostDTO>> findPosts(@PathVariable String id) {
+		List<PostDTO> list = service.findPosts(id);
+		return ResponseEntity.ok().body(list);
+	}
+
+	@PostMapping
+	public ResponseEntity<UserDTO> insert(@RequestBody UserDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> update(@PathVariable String id, @RequestBody UserDTO dto) {
+		dto = service.update(id, dto);
+		return ResponseEntity.ok(dto);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
