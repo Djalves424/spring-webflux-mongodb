1cbea26443c22ccc78afe9f58ec45ca739e5112a
Djalves424 <djalves424@gmail.com>
2024-09-09 16:34:40 -0300

    GET post by id

--- src/main/java/com/devsuperior/workshopmongo/controllers/PostController.java
+++ src/main/java/com/devsuperior/workshopmongo/controllers/PostController.java
@@ -1,16 +1,27 @@
 package com.devsuperior.workshopmongo.controllers;
 
+import com.devsuperior.workshopmongo.dto.PostDTO;
+import com.devsuperior.workshopmongo.dto.UserDTO;
 import com.devsuperior.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping(value = "/posts")
 public class PostController {
 
-	@Autowired
-	private PostService service;
+    @Autowired
+    private PostService service;
+
+    @GetMapping(value = "/{id}")
+    public Mono<ResponseEntity<PostDTO>> findById(@PathVariable String id) {
+        return service.findById(id).map(postDTO -> ResponseEntity.ok().body(postDTO));
+    }
 
 	/*@GetMapping(value = "/{id}")
 	public ResponseEntity<PostDTO> findById(@PathVariable String id) {
--- src/main/java/com/devsuperior/workshopmongo/services/PostService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/PostService.java
@@ -1,8 +1,12 @@
 package com.devsuperior.workshopmongo.services;
 
+import com.devsuperior.workshopmongo.dto.PostDTO;
+import com.devsuperior.workshopmongo.dto.UserDTO;
 import com.devsuperior.workshopmongo.repositories.PostRepository;
+import com.devsuperior.workshopmongo.services.exceptioons.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 @Service
 public class PostService {
@@ -10,10 +14,12 @@ public class PostService {
     @Autowired
     private PostRepository repository;
 
-	/*@Transactional(readOnly = true)
-	public PostDTO findById(String id) {
-		Post post = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
-		return new PostDTO(post);
+    public Mono<PostDTO> findById(String id) {
+        return repository.findById(id).map(existingPost -> new PostDTO(existingPost))
+                .switchIfEmpty(Mono.error(new ResourceNotFoundException("Recurso não encontrado")));
+    }
+
+	/*
 	}
 	
 	public List<PostDTO> findByTitle(String text) {
